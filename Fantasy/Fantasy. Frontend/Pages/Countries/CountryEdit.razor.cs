e2fd3b41e13fae881bbf1507a8f9fa77a1cacccb
ISAIAS HUMBERTO HERRERA PEREZ <ihherrera@soyucn.edu.co>
2025-02-08 17:28:09 -0500

    country crud

---
+++ Fantasy/Fantasy. Frontend/Pages/Countries/CountryEdit.razor.cs
@@ -0,0 +1,74 @@
+using CurrieTechnologies.Razor.SweetAlert2;
+using Fantasy.Frontend.Repositories;
+using Fantasy.Shared.Entities;
+using Fantasy.Shared.Resources;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+
+namespace Fantasy.Frontend.Pages.Countries;
+
+public partial class CountryEdit
+{
+    private Country? country;
+    private CountryForm? countryForm;
+
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+    [Inject] private IStringLocalizer<Literals> Localizer { get; set; } = null!;
+
+    // tomamos el id por parametro
+    [Parameter] public int Id { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        // traemos el objeto mediante el get
+        var responseHttp = await Repository.GetAsync<Country>($"api/countries/{Id}");
+
+        if (responseHttp.Error)
+        {
+            if (responseHttp.HttpResponseMessage.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                NavigationManager.NavigateTo("countries");
+            }
+            else
+            {
+                var messageError = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync(Localizer["Error"], messageError, SweetAlertIcon.Error);
+            }
+        }
+        else
+        {
+            country = responseHttp.Response;
+        }
+    }
+
+    private async Task EditAsync()
+    {
+        // tomamos el objeto para editarlo y lo enviamos actualizado al backend
+        var responseHttp = await Repository.PutAsync("api/countries", country);
+
+        if (responseHttp.Error)
+        {
+            var mensajeError = await responseHttp.GetErrorMessageAsync();
+            await SweetAlertService.FireAsync(Localizer["Error"], mensajeError, SweetAlertIcon.Error);
+            return;
+        }
+
+        Return();
+        var toast = SweetAlertService.Mixin(new SweetAlertOptions
+        {
+            Toast = true,
+            Position = SweetAlertPosition.BottomEnd,
+            ShowConfirmButton = true,
+            Timer = 3000
+        });
+        await toast.FireAsync(icon: SweetAlertIcon.Success, message: Localizer["RecordSavedOk"]);
+    }
+
+    private void Return()
+    {
+        countryForm!.FormPostedSuccessfully = true;
+        NavigationManager.NavigateTo("countries");
+    }
+}
